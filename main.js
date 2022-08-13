0cc9c189a898a98d2f264795ddf628947e52bc32
Thaiza Capelao <thaizacapelao@gmail.com>
2022-08-13 00:03:20 -0400

    otimizando o código

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 //document.querySelector('.tecla_pom').onclick = tocaSomPom;
@@ -7,4 +7,31 @@ function tocaSomPom() {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+//let = variavel
+//enquanto = while (loop) + uma condição que tenha fim
+//length = quantidade da lista = 9 (temos 9 teclas)
+//for = para
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string = `#xxx_${xxxxxxx}`
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+
+//o Console.log vai imprimir essa condição, até ela ser considerada
+//falsa, ou seja, quando for > que 9 = false    
+//contador++ = contador = contador +1;    
+
+console.log(contador);
+
+}
+
